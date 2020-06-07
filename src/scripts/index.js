ba517f50b2e026dcb2e614befa3d169a08ac4db0
truong nguyen <n.h.t.94.e@gmail.com>
2020-06-07 22:01:13 +0700

    update

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,5 @@
-import './../styles/main.scss';
-import './slick/slick';
+import "./../styles/main.scss";
+import "./slick/slick";
 require("./fontawesome.min.js");
 // import Parallax from 'parallax-scroll';
 // require('./scroll.js');
@@ -24,61 +24,97 @@ require("./fontawesome.min.js");
 // 	container.classList.toggle('opened');
 // };
 
-$('.carousel-wrapper').slick({
-	infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplaySpeed: 2000,
-    autoplay: true,
-    speed: 2000,
-    arrows: false
+$(".carousel-wrapper").slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplaySpeed: 2000,
+  autoplay: true,
+  speed: 2000,
+  arrows: false,
 });
 
-$('.product-carousel-wrapper').slick({
-    prevArrow: '<a href="#prevArrow" class="btn-arrow btn-arrow-left"><i class="fas fa-chevron-left"></i></a>',
-	nextArrow:'<a href="#nextArrow" class="btn-arrow btn-arrow-right"><i class="fas fa-chevron-right"></i></a>',
-    dots: true,
-	infinite: true,
-	slidesToShow: 4,
-	slidesToScroll: 4,
-	autoplay: false,
-    autoplaySpeed: 2000,
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: false
-            }
-        },
-        {
-            breakpoint: 800,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-                infinite: true,
-                dots: false
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                dots:false
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                dots:false
-            }
-        }
-    ]
+$(".carousel-slider").slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  autoplaySpeed: 3000,
+  autoplay: true,
+  pauseOnFocus: true,
+  pauseOnHover: true,
+  speed: 2000,
+  nextArrow: $(".bannerPrimary__slider > .next").get(0),
+  prevArrow: $(".bannerPrimary__slider > .previous").get(0),
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      },
+    },
+    {
+      breakpoint: 788,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 1044,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+  ],
 });
 
-
+$(".product-carousel-wrapper").slick({
+  prevArrow:
+    '<a href="#prevArrow" class="btn-arrow btn-arrow-left"><i class="fas fa-chevron-left"></i></a>',
+  nextArrow:
+    '<a href="#nextArrow" class="btn-arrow btn-arrow-right"><i class="fas fa-chevron-right"></i></a>',
+  dots: true,
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  autoplay: false,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false,
+      },
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: false,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+      },
+    },
+  ],
+});
